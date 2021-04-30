66f6c9b50598a2530f5d2c0e350c7ceb9796ac4c
fytta <rafa.esparza.1995@gmail.com>
2021-04-30 16:22:53 +0200

    updated

--- src/main/java/com/store/controllers/FilmController.java
+++ src/main/java/com/store/controllers/FilmController.java
@@ -5,17 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import com.store.models.Film;
 import com.store.services.FilmService;
 
-//@CrossOrigin() // Descomentar para aceptar peticiones *.*.*.*
+@CrossOrigin() // Descomentar para aceptar peticiones *.*.*.*
 @RestController
 @RequestMapping("/api/v1")
 public class FilmController {
